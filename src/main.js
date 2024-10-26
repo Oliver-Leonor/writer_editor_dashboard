c9c39a3234a9760cc3d7b9fe9f1f804e7632de86
Oliver <oliver.leonor90@gmail.com>
2024-10-26 08:44:39 +0800

    Initial commit: Vue.js project setup with Vuetify, Vue Router, Vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import VueQuillEditor from "vue-quill-editor";
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
 
-createApp(App).use(store).use(router).mount('#app')
+Vue.use(Vuetify);
+Vue.use(VueQuillEditor);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  vuetify: new Vuetify(),
+  render: (h) => h(App),
+}).$mount("#app");
